d34e4b97333eb96ff37a1ace6a4bccbe28d799f2
IshikaGarg787 <ishikagarg330@gmail.com>
2024-11-28 00:43:46 +0530

    replace

---
+++ coding-assignment/string/string_q03.py
@@ -0,0 +1,5 @@
+#3. Replace All Occurrences of a Substring
+#Problem: Replace all occurrences of "cat" with "dog" in the string "the cat sat on the cat".
+
+s = "the cat sat on the cat"
+print(s.replace("cat", "dog"))
