d797a4843e2e8b81cca3f4e4015f61ba810b6356
IshikaGarg787 <ishikagarg330@gmail.com>
2024-11-28 00:57:47 +0530

    start_or_end_substring

---
+++ coding-assignment/string/string_q11.py
@@ -0,0 +1,5 @@
+#11. Check if a String Starts or Ends with a Substring
+#Problem: Check if the string "hello world" starts with "hello" and ends with "world".
+
+s = "hello world"
+print(s.startswith("hello") and s.endswith("world"))
