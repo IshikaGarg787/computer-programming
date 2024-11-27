eb3ec3085b24fa5287860529a8fa85d89163037c
IshikaGarg787 <ishikagarg330@gmail.com>
2024-11-27 23:26:06 +0530

    merge_as_list

---
+++ coding-assignment/dict/dict_q08.py
@@ -0,0 +1,7 @@
+#9. Merge Dictionaries into a List of Dictionaries
+#Problem: Merge two or more dictionaries into a list of dictionaries.
+
+dict1 = {'a': 1, 'b': 2}
+dict2 = {'c': 3, 'd': 4}
+merged = [dict1, dict2]
+print(merged)
