2a6441ab2a35b5f550e6cb4df841218d38d11d28
IshikaGarg787 <ishikagarg330@gmail.com>
2024-11-28 01:38:16 +0530

    merge_multiple_tuples

---
+++ coding-assignment/tuple/tuple_q09.py
@@ -0,0 +1,8 @@
+#9. Merge Multiple Tuples into One
+#Problem: Merge the tuples (1, 2), (3, 4), and (5, 6) into one tuple.
+
+t1 = (1, 2)
+t2 = (3, 4)
+t3 = (5, 6)
+merged = t1 + t2 + t3
+print(merged)
