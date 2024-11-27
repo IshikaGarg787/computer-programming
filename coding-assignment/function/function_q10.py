9f4371ad1d97b5f2cc391183d269542fb4a6416c
IshikaGarg787 <ishikagarg330@gmail.com>
2024-11-27 23:48:44 +0530

    prime_numbers

---
+++ coding-assignment/function/function_q10.py
@@ -0,0 +1,14 @@
+#10. Find Prime Numbers in a Range
+#Write a function that finds all prime numbers in a given range.
+
+def primes_in_range(start, end):
+    primes = []
+    for num in range(start, end + 1):
+        if num > 1:
+            for i in range(2, num):
+                if num % i == 0:
+                    break
+            else:
+                primes.append(num)
+    return primes
+print(primes_in_range(10, 30))
