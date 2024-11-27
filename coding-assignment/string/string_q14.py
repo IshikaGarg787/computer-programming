4730cbd35d031f0310f3288c67f56733e62a039a
IshikaGarg787 <ishikagarg330@gmail.com>
2024-11-28 01:04:34 +0530

    last_occurrences

---
+++ coding-assignment/string/string_q14.py
@@ -0,0 +1,5 @@
+#14. Find the Last Occurrence of a Substring
+#Problem: Find the last occurrence of "fox" in the string "The quick brown fox jumped over the lazy fox".
+
+s = "The quick brown fox jumped over the lazy fox"
+print(s.rfind("fox"))
