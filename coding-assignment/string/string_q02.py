72094ed2393e21cb8d8d7b017d3809f53e152dcc
IshikaGarg787 <ishikagarg330@gmail.com>
2024-11-28 00:42:11 +0530

    count_occurences

---
+++ coding-assignment/string/string_q02.py
@@ -0,0 +1,6 @@
+#2. Count the Number of Occurrences of a Substring
+#Problem: Count how many times the substring "ab" appears in "ababab".
+
+s = "ababab"
+substring = "ab"
+print(s.count(substring))
