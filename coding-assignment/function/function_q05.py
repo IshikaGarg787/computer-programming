8ec8ccf0658dcbca6b747c71e456148ab7c5d34c
IshikaGarg787 <ishikagarg330@gmail.com>
2024-11-27 23:38:41 +0530

    palindrome_or_not

---
+++ coding-assignment/function/function_q05.py
@@ -0,0 +1,6 @@
+#7. Check if a String is Palindrome
+#Write a function that checks if a given string is a palindrome.
+
+def is_palindrome(s):
+    return s == s[::-1]
+print(is_palindrome("madam"))
